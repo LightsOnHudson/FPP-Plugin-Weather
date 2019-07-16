fb74c361c4368c3761b8bab1b75e23121779f6a7
patdelaney <pat_delaney@colonydrive.com>
2019-07-15 19:07:52 -0500

    Add files via upload
    added .sh files that can be used by cron jobs to get local weather and north pole weather  initial commit

---
+++ getLocalWeather.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#Script to run local weather from playlist
+#
+#
+/usr/bin/php /home/fpp/media/plugins/FPP-Plugin-Weather/getWEATHER.php   
---
+++ getNorthPoleWeather.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#Script to run North Pole weather from playlist
+#
+#
+/usr/bin/php /home/fpp/media/plugins/FPP-Plugin-Weather/getNorthPole.php   
