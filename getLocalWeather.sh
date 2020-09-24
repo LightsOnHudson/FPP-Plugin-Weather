3d7405a113a21b91a93d1fa52b9f57347151e173
Poporacer <46263877+Poporacer@users.noreply.github.com>
2020-09-23 18:45:08 -0600

    Update getLocalWeather.sh
    Make display immediate for now, will add option later.

--- getLocalWeather.sh
+++ getLocalWeather.sh
@@ -3,4 +3,5 @@
 #Script to run local weather from playlist
 #
 #
-/usr/bin/php /home/fpp/media/plugins/FPP-Plugin-Weather/getWEATHER.php   
+/usr/bin/php /home/fpp/media/plugins/FPP-Plugin-Weather/getWEATHER.php  
+/usr/bin/php /home/fpp/media/plugins/FPP-Plugin-Matrix-Message/matrix.php
